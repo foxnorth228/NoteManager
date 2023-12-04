75feb99e9f7731b50d78351cc2eaceacbab6d070
Alex Khitry <khitrii03@gmail.com>
2023-12-04 15:49:03 +0300

    feat: add store

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import App from '@pages/App';
+import store from '@src/store';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
