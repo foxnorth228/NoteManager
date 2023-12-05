5a18424dfe12a9a008db3e3e45690be95eb276fb
Alex Khitry <khitrii03@gmail.com>
2023-12-05 20:20:59 +0300

    refactor: some minor changes

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,10 +1,22 @@
 import Note from '@components/Note';
+import { GlobalStyles } from '@mui/material';
 import React from 'react';
-import './style.scss';
+
+const globalStyles = (
+  <GlobalStyles
+    styles={() => ({
+      ':root': { boxSizing: 'border-box' },
+      '*': { boxSizing: 'inherit' },
+      ':before': { boxSizing: 'inherit' },
+      ':after': { boxSizing: 'inherit' },
+    })}
+  />
+);
 
 const App = () => {
   return (
     <div>
+      {globalStyles}
       Hello world
       <Note></Note>
     </div>
